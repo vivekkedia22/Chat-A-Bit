ad5658ffd12e082afa9fb1a8fa0272007aee7922
vivekkedia22 <vivekkedia22@gmail.com>
2024-07-12 09:12:44 +0530

    adding middlewares, setup cross env

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,50 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
 
+//dotEnv connfig
+dotenv.config();
 
 //create express app
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello from Server")
-})
+//morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 
+//helmet
+app.use(helmet());
+
+//parse json request body
+app.use(express.json());
+
+//Parse json request body
+app.use(express.urlencoded({ extended: true }));
+
+//sanitize request data
+app.use(mongoSanitize());
+
+//enable cookie Parser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(fileUpload({ useTempFiles: true }));
+
+//cors
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send("Hello from Server");
+});
 
 export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,10 @@
-import dotenv from "dotenv";
 import app from "./app.js"
 
-//dotEnv connfig
-dotenv.config();
 
 //env variables
-
 const PORT = process.env.PORT || 8000;
 
+console.log(process.env.NODE_ENV);
 app.listen(PORT, () => {
   console.log(`Server connected at port ${PORT}`);
 });
