a8cbbaf9317521b8895fef7e792642722d367d2d
vivekkedia22 <vivekkedia22@gmail.com>
2024-07-12 17:59:18 +0530

    custom logger

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
 import app from "./app.js"
-
+import logger from "./config/logger.config.js"
 
 //env variables
 const PORT = process.env.PORT || 8000;
 
 console.log(process.env.NODE_ENV);
 app.listen(PORT, () => {
-  console.log(`Server connected at port ${PORT}`);
+  logger.info(`Server connected at port ${PORT}`);
 });
